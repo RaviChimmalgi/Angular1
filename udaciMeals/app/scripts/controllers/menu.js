4a31554616359fa0e2374cb9c7f2150704174b00
Ravi Chimmalgi <ravi.chimmalgi@gmail.com>
2016-07-19 15:21:38 -0500

    feat: controllers and directives quiz

---
+++ udaciMeals/app/scripts/controllers/menu.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name udaciMealsApp.controller:MenuCtrl
+ * @description
+ * # MenuCtrl
+ * Controller of the udaciMealsApp
+ */
+angular.module('udaciMealsApp')
+  .controller('MenuCtrl', function () {
+    this.id ='strawberry-pudding';
+    this.name = 'Strawberry-pudding';
+    this.img = 'strawberry-pudding.jpg';
+    this.rating = 5;
+  });
